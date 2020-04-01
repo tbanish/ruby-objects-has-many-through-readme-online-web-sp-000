7fd1162ef397f93e065e4b765e9e841863cb37ea
Tom Banish <tombanish@gmail.com>
2020-04-01 20:20:24 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -16,4 +16,16 @@ class Customer
   def new_meal(waiter, total, tip=0)
     Meal.new(waiter, self, total, tip)
   end
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self 
+    end
+  end
+  
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
 end
